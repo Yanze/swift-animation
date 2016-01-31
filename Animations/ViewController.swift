9231985f92e8f3008c2d685777a55d14be45aa51
Yanze LIU <yanzeliu.france@gmail.com>
2016-01-30 16:56:53 -0800

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by yanze on 1/30/16.
+//  Copyright © 2016 River Inn Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
